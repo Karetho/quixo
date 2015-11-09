39d18b0398c063abc34d301639710b2c34ac82e3
Karetho <richard_lim@hotmail.fr>
2015-11-09 18:36:09 +0100

    Test

--- test/TestJoueurHumain.java
+++ test/TestJoueurHumain.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 /**
  * Created by Roland on 09-Nov-15.
  */
 public class TestJoueurHumain {
+    // test si le joueur a gagner
+    @Test
+    public void testJoueurGagne(){
+    }
+    // test si le cube est bien celui du joueur
+    @Test
+    public void testChangerSigne(){
+    }
 }
