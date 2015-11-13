8c46e03e9eb8c7f90facb48316c4d722be4d1d00
Karetho <richard_lim@hotmail.fr>
2015-11-13 10:20:50 +0100

    Test

---
+++ src/controller/Joueur.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Roland on 13-Nov-15.
+ */
+public class Joueur {
+}
---
+++ src/model/Joueur.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Roland on 13-Nov-15.
+ */
+public class Joueur {
+}
