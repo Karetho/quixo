4be3b3328c8331d74624c9e0f0521a49e67a9a08
Karetho <richard_lim@hotmail.fr>
2015-11-27 11:18:42 +0100

    Test de la méthode choixPossibles dans Plateau terminé

--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -1,6 +1,7 @@
 package model;
 
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -20,21 +21,51 @@ public class Plateau {
 
 
     public List choixPossible(int i, int j) {
+        List retour = new LinkedList<>();
         //Le cube déplacé est dans la bordure du haut
-        if(i == 0 && j != 0){
-
+        if(i == 0 && (j != 0 || j != 4)){
+            retour.add(new int[]{4,j});
+            retour.add(new int[]{0,4});
+            retour.add(new int[]{0,0});
+        }
+        //Le cube déplacé est dans la bordure du bas
+        else if(i == 4 && (j != 4 || j != 0)){
+            retour.add(new int[]{0,j});
+            retour.add(new int[]{0,4});
+            retour.add(new int[]{4,4});
         }
         //Le cube déplacé est dans la bordure de gauche
-        else if(i != 0 && j == 0){
-
+        else if((i != 0 || i != 4) && j == 0){
+            retour.add(new int[]{i,4});
+            retour.add(new int[]{4,0});
+            retour.add(new int[]{0,0});
         }
-        //Le cube déplacé est
-        else if(i != 4 && j == 4){
-
+        //Le cube déplacé est dans la bordure droite
+        else if((i != 0 ||i != 4) && j == 4){
+            retour.add(new int[]{i,0});
+            retour.add(new int[]{0,4});
+            retour.add(new int[]{4,4});
         }
-        else if(i == 4 && j != 4){
-
+        //Le cube déplacé est dans le corner haut gauche (0,0)
+        else if(i == 0 && j == 0){
+            retour.add(new int[]{0,4});
+            retour.add(new int[]{4,4});
         }
-        return null;
+        //Le cube déplacé est dans le corner haut gauche (0,0)
+        else if(i == 0 && j == 4){
+            retour.add(new int[]{0,0});
+            retour.add(new int[]{4,4});
+        }
+        //Le cube déplacé est dans le corner haut gauche (0,0)
+        else if(i == 4 && j == 0){
+            retour.add(new int[]{0,0});
+            retour.add(new int[]{0,4});
+        }
+        //Le cube déplacé est dans le corner haut gauche (0,0)
+        else if(i == 4 && j == 4){
+            retour.add(new int[]{0,4});
+            retour.add(new int[]{4,0});
+        }
+        return retour;
     }
 }
--- test/TestPlateau.java
+++ test/TestPlateau.java
@@ -2,47 +2,123 @@
 import model.Joueurs.Humains.Humains;
 import model.Plateau;
 import org.junit.*;
-
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
 
 /**
  * Created by Roland on 26-Nov-15.
  */
 public class TestPlateau {
+    Humains joueur;
+    Plateau plateau;
+    @Before
+    public void setUp(){
+        joueur = new Humains();
+        plateau = new Plateau();
+    }
+
     //Teste si le joueur a gagne
     @Test
     public void testSiJoueurGagne(){
-        Humains joueur = new Humains();
-        Plateau plateau = new Plateau();
+        setUp();
     }
     //Teste le décalement des cubes sur la ligne
     @Test
     public void testDecaleCases(){
-        Plateau plateau = new Plateau();
+        setUp();
 
     }
     //Montre les choix possibles que peux avoir le joueur
     @Test
-    public void testChoixPossible(){
+    public void testChoixPossibleCorner1(){
+        //Teste le corner en haut a droite
+        setUp();
         List liste = new LinkedList<>();
         liste.add(new int[]{0,4});
         liste.add(new int[]{4,0});
         //liste.add(new int[]{0,4});
-        Humains joueur = new Humains();
-        Plateau plateau = new Plateau();
         //Premier test sur un corner
         joueur.prendreCube(0,0);
         Assert.assertEquals(liste,plateau.choixPossible(0,0));
-        //Deuxieme test sur une case du milieu
-        List liste1 = new LinkedList<>();
-        liste1.add(new int[]{2,4});
-        liste1.add(new int[]{4,0});
-        liste1.add(new int[]{0,0});
+    }@Test
+    public void testChoixPossibleCorner2(){
+        //Teste le corner en bas a gauche
+        setUp();
+        List liste = new LinkedList<>();
+        liste.add(new int[]{4,0});
+        liste.add(new int[]{0,4});
+        //liste.add(new int[]{0,4});
+        //Premier test sur un corner
+        joueur.prendreCube(4,4);
+        Assert.assertEquals(liste,plateau.choixPossible(4,4));
+    }
+    @Test
+    public void testChoixPossibleCorner3(){
+        //Teste le corner en bas a gauche
+        setUp();
+        List liste = new LinkedList<>();
+        liste.add(new int[]{4,4});
+        liste.add(new int[]{0,4});
+        //liste.add(new int[]{0,4});
+        //Premier test sur un corner
+        joueur.prendreCube(4,0);
+        Assert.assertEquals(liste,plateau.choixPossible(4,0));
+
+    }
+    @Test
+    public void testChoixPossibleCorner4(){
+        //Teste le corner en haut a droite
+        setUp();
+        List liste = new LinkedList<>();
+        liste.add(new int[]{4,4});
+        liste.add(new int[]{4,0});
+        //liste.add(new int[]{0,4});
+        //Premier test sur un corner
+        joueur.prendreCube(0,4);
+        Assert.assertEquals(liste,plateau.choixPossible(0,4));
+
+    }
+    @Test
+    public void testChoixPossibleGauche(){
+        setUp();
+        //Deuxieme test sur une case du milieu sans les corners
+        List liste = new LinkedList<>();
+        liste.add(new int[]{2,4});
+        liste.add(new int[]{4,0});
+        liste.add(new int[]{0,0});
         joueur.prendreCube(2,0);
         Assert.assertEquals(liste,plateau.choixPossible(2,0));
+    }
+    @Test
+    public void testChoixPossibleDroite(){
+        setUp();
+        //Deuxieme test sur une case du milieu
+        List liste = new LinkedList<>();
+        liste.add(new int[]{0,2});
+        liste.add(new int[]{0,4});
+        liste.add(new int[]{4,4});
+        joueur.prendreCube(2,4);
+        Assert.assertEquals(liste,plateau.choixPossible(2,4));
+    }@Test
+    public void testChoixPossibleHaut(){
+        setUp();
+        //Deuxieme test sur une case du milieu
+        List liste = new LinkedList<>();
+        liste.add(new int[]{4,2});
+        liste.add(new int[]{0,4});
+        liste.add(new int[]{0,0});
+        joueur.prendreCube(0,2);
+        Assert.assertEquals(liste,plateau.choixPossible(0,2));
+    }@Test
+    public void testChoixPossibleBas(){
+        setUp();
+        //Deuxieme test sur une case du milieu
+        List liste = new LinkedList<>();
+        liste.add(new int[]{0,2});
+        liste.add(new int[]{0,4});
+        liste.add(new int[]{4,4});
+        joueur.prendreCube(4,2);
+        Assert.assertEquals(liste,plateau.choixPossible(4,2));
     }
 }
